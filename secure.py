3e5e102e71b466882055b92c0a59359cdaa6c817
luopeixiong <luopx@hffss.com>
2018-07-12 11:03:36 +0800

    '加入汽车之家'

--- secure.py
+++ secure.py
@@ -5,9 +5,4 @@ SECRET_KEY = 'your SECRET_KEY'
 REDIS_URL = 'redis://用户名:密码@host:port'
 FIXED = True
 
-APP_ID = '11458591'
-API_KEY = 'cRw83DXbuV6BWeVpoSzxaTrf'
-SECRET_KEY = 'aD7s3pLzbYYQnl92KH5gLjakM7MdTNxS'
-REDIS_URL = 'redis://luopx:Haifeng.123@10.1.12.80:6379'
-FIXED = True
 
